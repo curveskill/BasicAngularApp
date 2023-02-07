5f8b364371f3119aadeefde32a0450dc24d82b80
curveskills <curveskills@gmail.com>
2023-02-07 23:06:27 +0530

    building block explained

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BasicAngularApp';
+  constructor(){}
+  title:string = 'BasicAngularApp';
 }
